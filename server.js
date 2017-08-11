0800d432d8244e6fe4a979285279e299bc49b5c4
SandipBhogulkar <30897018+SandipBhogulkar@users.noreply.github.com>
2017-08-10 23:37:40 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/article-two',function(req,res){
 });
 
 app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-three.html'))
+    res.sendFile(path.join(__dirname,'ui','artcle-three.html'))
 });
 
 app.get('/ui/madi.png', function (req, res) {
