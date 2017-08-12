1b90330f61d306665ef39dc606c089a3337f9bec
SandipBhogulkar <30897018+SandipBhogulkar@users.noreply.github.com>
2017-08-12 01:59:36 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
 
 
-var element=document.getElementById('main-text');
+/*var element=document.getElementById('main-text');
 
-element.innerHTML='Hey Sandy';
+element.innerHTML='Hey Sandy';*/
 
 //move image
 var img=document.getElementById('madi');
@@ -20,3 +20,17 @@ img.onclick= function(){
     var interval=setInterval(moveRight, 50);
     //img.style.marginLeft='100px';
 };
+
+//make counter coading
+var counter=0;
+var button=document.getElementById('counter');
+button.onclick=function(){
+    //make a request to the counter endpoint
+    
+    //capture the response and store in variable
+    
+    //render the variable in correct span
+    counter=counter+1;
+    var span =document.getElementById('count');
+    span.innerHTML=counter.toString();
+}
