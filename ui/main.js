ed4bfee7ce7483a233a32355c890abf517fb13d0
SandipBhogulkar <30897018+SandipBhogulkar@users.noreply.github.com>
2017-08-12 01:17:49 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,14 @@ element.innerHTML='Hey Sandy';
 //move image
 var img=document.getElementById('madi');
 alert('get'+img);
+
+var marginLeft=0;
+
+function moveRight(){
+    marginLeft= marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
 img.onclick= function(){
     var interval=setInterval(moveRight, 100);
     //img.style.marginLeft='100px';
